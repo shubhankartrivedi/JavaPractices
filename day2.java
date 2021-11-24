6248b178445ad23309dffd5ca95dc4b67c8bec62
Shubhankar Trivedi <78972053+shubhankartrivedi@users.noreply.github.com>
2021-11-24 15:19:17 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class day2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your Principle Amount:");
+        int p = sc.nextInt();
+        System.out.println("Enter your Rate of Interest:");
+        int r = sc.nextInt();
+        System.out.println("Enter your Time given:");
+        int t = sc.nextInt();
+        SI(p,r,t);
+
+    }
+// Using New Custom Method :
+    public static void SI(int p, int r, int t) {
+        double si = (p*r*t)/100;
+        System.out.println("Your Simple Interest/Profit in "+t+" Year is: "+si);
+    }
+}
+
+/*
+By Shubhankar Trivedi
+Date: 24-11-2021
+ */
